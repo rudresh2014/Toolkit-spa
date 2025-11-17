b74655ef21828eb89b5f14267fb234841c6ca2a5
Rudresh <rudresh9741740@gmail.com>
2025-11-17 17:13:09 +0530

    Updated PWA support

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,7 @@
 self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open("toolkit-cache").then((cache) => {
-      return cache.addAll([
-        "/",
-        "/index.html",
-        "/style.css",
-        "/home.js",
-        "/router.js",
-        "/supabaseClient.js",
-        "/assets/icon.png"
-      ]);
-    })
-  );
+  self.skipWaiting();
 });
 
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener("activate", (e) => {
+  clients.claim();
 });
